205a0bf573dfea5ea5202d0c8c5e8a8dad425312
David Hernandez <david.hernandez.mendez@gmail.com>
2018-07-12 17:09:48 -0500

    Vue-Ds
    Inicio, agrega vue.

---
+++ dashboard/src/views/Drawer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="drawer">
+        
+        <md-app>
+            <md-app-toolbar class="md-primary">
+                <span>Dashboards.</span>
+                <md-badge class="md-primary" md-content="12">
+                    <md-button class="md-icon-button">
+                        <md-icon>home</md-icon>
+                    </md-button>
+                </md-badge>
+            </md-app-toolbar>
+        </md-app>
+        <p>Hola mundo.</p>
+    </div>
+</template>
+<script>
+export default {
+    name: "Drawer"
+}
+</script>
